aadaeb465e28a3bc6ed151a630e963702b033885
zml <629092+zml2008@users.noreply.github.com>
2024-11-10 04:53:05 +0000

    mergiraf: add 0.2.0 bottle.

--- Formula/mergiraf.rb
+++ Formula/mergiraf.rb
@@ -5,6 +5,13 @@ class Mergiraf < Formula
   sha256 "fb496ef6470091dd9f133aeea18852067ce82c0dbccbdf1390dd66aec5e60e03"
   license "GPL-3.0-only"
 
+  bottle do
+    root_url "https://ghcr.io/v2/zml2008/abbuilds"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f317ea632c2899116c25794ad80f40cd4c80f57a21973dc6b398322dc9922196"
+    sha256 cellar: :any_skip_relocation, ventura:       "491d5ed2e88c32ff96eabb4e55c1ca50f11952b09a896cd126cd5799aacced78"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "89bdd4b062b33d698a2e57621fd0db71ced87fb8e066c756814bdc28b80ba7cf"
+  end
+
   depends_on "rust" => :build
 
   def install
