9a14b574084591b7b19f1341133d49a9dabd8993
zml <zml@stellardrift.ca>
2024-11-09 20:10:26 -0800

    mergiraf: remove unnecessary livecheck block

--- Formula/mergiraf.rb
+++ Formula/mergiraf.rb
@@ -5,11 +5,6 @@ class Mergiraf < Formula
   sha256 "fb496ef6470091dd9f133aeea18852067ce82c0dbccbdf1390dd66aec5e60e03"
   license "GPL-3.0-only"
 
-  livecheck do
-    url "https://codeberg.org/mergiraf/mergiraf/releases"
-    regex(%r{href=.*?mergiraf/archive/v(\d+(?:\.\d+)+)\.t}i)
-  end
-
   depends_on "rust" => :build
 
   def install
